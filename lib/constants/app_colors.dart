8918ed2df57b72c830732a192d61f83f6b201c5b
Vicente Souza <vicentesouza.dev@gmail.com>
2026-08-12 15:54:11 -0300

    add colors

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF05150e);
+  static const Color surface = Color(0xFF0a291d);
+
+  static const Color darkDeep = Color(0xFF082117);
+  static const Color darkDeepest = Color(0xFF0f3e2b);
+
+  static const Color primary = Color(0xFF46d29a);
+  static const Color primaryDark = Color(0xFF2db981);
+  static const Color primaryDarker = Color(0xFF239064);
+  static const Color primaryDarkest = Color(0xFF196748);
+  static const Color primaryLight = Color(0xFF6fdcb1);
+
+  static const Color textLight = Color(0xFFeafaf4);
+  static const Color textMuted = Color(0xFF98e6c7);
+  static const Color textLightest = Color(0xFFc1f0dd);
+}
